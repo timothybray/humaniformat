94461f2c77e521d5dc8716824483d429b09da72c
Timothy bray <67018426+timothybray@users.noreply.github.com>
2024-02-06 16:58:33 -0600

    Update human_parse.cpp

--- src/human_parse.cpp
+++ src/human_parse.cpp
@@ -226,22 +226,22 @@ human_parse::human_parse(){
   suffixes.insert("dmd");
   suffixes.insert("cme");
   suffixes.insert("esq");
-  suffixes.insert("o.f.m.cap");
-  suffixes.insert("o.p.");
-  suffixes.insert("f.n.");
-  suffixes.insert("c.m.");
-  suffixes.insert("o.s.b.");
-  suffixes.insert("i.v.e.");
-  suffixes.insert("c.ss.r");
-  suffixes.insert("s.d.d.");
-  suffixes.insert("m.n.m.");
-  suffixes.insert("c.s.");
-  suffixes.insert("c.j.m.");
-  suffixes.insert("s.chr.");
-  suffixes.insert("l.c.");
-  suffixes.insert("o.c.d.");
-  suffixes.insert("j.c.l.");
-  suffixes.insert("v.f.");
+  suffixes.insert("ofmcap");
+  suffixes.insert("op");
+  suffixes.insert("fn");
+  suffixes.insert("cm");
+  suffixes.insert("osb");
+  suffixes.insert("ive");
+  suffixes.insert("cssr");
+  suffixes.insert("sdd");
+  suffixes.insert("mnm");
+  suffixes.insert("cs");
+  suffixes.insert("cjm");
+  suffixes.insert("schr");
+  suffixes.insert("lc");
+  suffixes.insert("ocd");
+  suffixes.insert("jcl");
+  suffixes.insert("vf");
   
   // Compounds
   compounds.insert("vere");
